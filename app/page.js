6225753121d1822abfbedb1aa2121de41a692a1f
AbhayMishra11 <55750241+AbhayMishra11@users.noreply.github.com>
2024-12-03 18:42:52 +0530

    Nothing, only minor fixes

--- app/page.js
+++ app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
     <>
       <main>
         <section className=" min-h-[100vh] grid grid-cols-2">
-          <div className="relative flex flex-col gap-5 items-center pt-[44vh] ml-[6vw]">
+          <div className="relative flex flex-col gap-11 items-center pt-64 ml-[6vw]">
             <h1 className={`${poppinsBold.className} text-7xl text-wrap text-[#d2e823] `}>Everything you are. In one, simple link in bio.</h1>
             <p className="text-wrap text-xl text-[#d2e823]">Join 50M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
 
