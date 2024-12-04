8f03f3af6264ef8fa0119b2468115b84fcf4bbd5
AbhayMishra11 <55750241+AbhayMishra11@users.noreply.github.com>
2024-12-04 16:51:32 +0530

    Adding input feature to generate page

--- app/generate/page.js
+++ app/generate/page.js
@@ -3,13 +3,36 @@ import Image from 'next/image'
 
 const Generate = () => {
   return (
-    <div className='min-h-screen grid grid-cols-2'>
-      <div className="col1">
-ee
+    <div className='h-full grid grid-cols-2 bg-[#225abf]'>
+      <div className="col1 flex flex-col gap-20 justify-center items-center">
+        <h1 className='font-bold text-5xl text-white'>Create Your BitTree</h1>
+        <div className='flex flex-col gap-9'>
+          <div className="items">
+            <h1 className='font-semibold text-2xl text-white'>Step 1: Claim Your Handle</h1>
+            <div className="mx-4 mt-6">
+              <input className='px-24 py-6 rounded-xl focus:outline-pink-600' type="text" placeholder='Choose a handle' />
+            </div>
+          </div>
+
+          <div className="items">
+            <h1 className='font-semibold text-2xl text-white'>Step 2: Add Links</h1>
+            <div className="mx-4 flex flex-col gap-6 mt-6">
+              <input className='px-24 py-6 rounded-xl focus:outline-pink-600' type="text" placeholder='Enter link text' />
+              <input className='px-24 py-6 rounded-xl focus:outline-pink-600' type="text" placeholder='Enter link' />
+            </div>
+          </div>
+
+          <div className="items">
+            <h1 className='font-semibold text-2xl text-white'>Step 3: Add a Picture and Finalize</h1>
+            <div className="mx-4 mt-6">
+              <input className='px-24 py-6 rounded-xl focus:outline-pink-600' type="text" placeholder='Add picture' />
+            </div>
+          </div>
+        </div>
       </div>
-      
+
       <div className="col2">
-     <img src="/Banner.png" alt="Banner" className='w-full' />
+        <img src="/Banner.png" alt="Banner" className='w-full' />
       </div>
     </div>
   )
